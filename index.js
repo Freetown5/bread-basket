ff5550e2cee22ca17613bec2d89cb0fc6d8cdd53
Isa Conteh <iconteh21@gmail.com>
2020-10-25 19:59:08 -0400

    initialized ejs in project

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const express = require("express");
 const app = express();
 
 app.listen(3000, () => console.log("Server Up and running"));
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.send('Page running');
+});
