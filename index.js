a2ca7dbbf612365b00811f24e95b9a2944f420f4
Isa Conteh <iconteh21@gmail.com>
2020-10-25 20:08:24 -0400

    added render for ejs

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 
 app.listen(3000, () => console.log("Server Up and running"));
 app.set("view engine", "ejs");
+app.use("/static", express.static("public"))
 
 app.get('/', (req, res) => {
-    res.send('Page running');
+    res.render('landing.ejs');
 });
