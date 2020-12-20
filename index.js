59fa0f805364ae9beb6d0ac9709b704b2aace601
Isa Conteh <iconteh21@gmail.com>
2020-12-20 17:57:42 -0500

    enabled use of static files in ejs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.listen(3000, () => console.log("Server Up and running"));
 app.set("view engine", "ejs");
-app.use("/static", express.static("public"))
+app.use(express.static("static"));
 
 app.get('/', (req, res) => {
     res.render('landing.ejs');
