3f0096990cb871f77944f20e89df832ad55dd0b2
Isa Conteh <iconteh21@gmail.com>
2020-12-26 17:06:24 -0500

    attempting to initialize connection of database to app

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.API_USERNAME}:${process.env.API_KEY}@bread-basket.lfwrb.mongodb.net/${process.env.API_APPNAME}?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(3000, () => console.log("Server Up and running"));
 app.set("view engine", "ejs");
@@ -11,4 +16,8 @@ app.get('/', (req, res) => {
 
 app.get('/buildings', (req, res) => {
     res.render('buildings.ejs');
+});
+
+client.connect(err => {
+    const collection = client.db("main").collection("buildings");
 })
